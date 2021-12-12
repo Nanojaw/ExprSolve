6ca8e5ac6c58e28ac5912e512e2ee74587498410
XpulseIV <malteaxner363@gmail.com>
2021-12-12 15:40:30 +0100

    s

---
+++ ExprSolve/Expression.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace ExprSolve
+{
+    public class Expression
+    {
+        private Dictionary<char, int> _unknowns = new();
+
+        public Expression(string expr)
+        {
+            foreach (char c in expr)
+            {
+                
+            }
+        }
+    }
+}
