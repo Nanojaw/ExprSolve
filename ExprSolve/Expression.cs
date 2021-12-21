290df40fed266b5d1891e2c8c59166fb302687bb
MuffenDuffen <reg_i@wext.se>
2021-12-21 16:47:28 +0100

    linux and started on expression

--- ExprSolve/Expression.cs
+++ ExprSolve/Expression.cs
@@ -3,9 +3,30 @@ using System.Collections.Generic;
 
 namespace ExprSolve
 {
+    public class Action
+    {
+        enum type
+        {
+            add,
+            sub,
+            mult,
+            div,
+            pow,
+            sqrt
+        }
+
+        int v1pos;
+        int v2pos;
+
+        int outpos;
+    }
+
     public class Expression
     {
-        public Expression(string expr, Dictionary<char, int?> unknowns)
+        Action[] actions;
+
+        int[] memory;
+        public Expression(string expr)
         {
             
         }
--- ExprSolve/Program.cs
+++ ExprSolve/Program.cs
@@ -1,24 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ExprSolve
 {
-    internal static class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var exprString = args[0];
-
-            // Create dictionary of all unknown values and set them to null
-            Dictionary<char, int?> unknowns = new();
-
-            foreach (var c in exprString)
-            {
-                if (!unknowns.ContainsKey(c) && char.IsLetter(c)) unknowns.Add(c, null);
-            }
-            
-            var expression = new Expression(exprString, unknowns);
+            var test = new Expression("3x+2x");
         }
     }
 }
